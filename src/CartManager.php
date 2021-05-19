d5b438d6cd58552ea5296f39dd6fa75789eadde2
Filip Ganyicz <ganyicz.filip@gmail.com>
2021-05-19 19:40:35 +0200

    Implement quantity and total

--- src/CartManager.php
+++ src/CartManager.php
@@ -2,6 +2,49 @@
 
 namespace Ganyicz\Cart;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+
 class CartManager
 {
+    private Collection $items;
+
+    public function __construct()
+    {
+        $this->items = collect(session('cart.items'));
+    }
+
+    public function total()
+    {
+        return $this->items->sum(fn ($item) =>
+            $item['model']->price * $item['quantity']
+        );
+    }
+    
+    public function add(Model $model, int $quantity = 1)
+    {
+        $existingItemIndex = $this->items->search(fn ($item) => $item['model']->is($model));
+        $existingItem = $existingItemIndex === false ? null : $this->items[$existingItemIndex];
+
+        if ($existingItem) {
+            $this->items->put($existingItemIndex, [
+                'model' => $existingItem['model'],
+                'quantity' => $existingItem['quantity'] + $quantity,
+            ]);
+        } else {
+            $this->items->add([
+                'model' => $model,
+                'quantity' => $quantity,
+            ]);
+        }
+
+        $this->update();
+    }
+
+    public function update()
+    {
+        session()->put('cart', [
+            'items' => $this->items,
+        ]);
+    }
 }
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -20,6 +20,37 @@ class ExampleTest extends TestCase
         $this->assertNotEmpty(session('cart'));
         $this->assertCount(1, session('cart.items'));
     }
+
+    /** @test */
+    public function quantity_can_be_specified()
+    {
+        $product = Product::create(['price' => 100_00]);
+        
+        Cart::add($product, 2);
+
+        $this->assertEquals(2, session('cart.items')[0]['quantity']);
+    }
+
+    /** @test */
+    public function total_is_calculated()
+    {
+        $product = Product::create(['price' => 100_00]);
+        
+        Cart::add($product, 2);
+
+        $this->assertEquals(200_00, Cart::total());
+    }
+
+    /** @test */
+    public function adding_existing_model_increments_quantity()
+    {
+        $product = Product::create(['price' => 100_00]);
+        
+        Cart::add($product);
+        Cart::add($product);
+
+        $this->assertEquals(2, session('cart.items')[0]['quantity']);
+    }
 }
 
 class Product extends Model 
