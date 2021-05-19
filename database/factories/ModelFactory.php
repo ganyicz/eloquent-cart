d51001aa90d7d79bcf69def97490e2e22ad807dd
Filip Ganyicz <ganyicz.filip@gmail.com>
2021-05-19 18:13:32 +0200

    Run package config

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Ganyicz\Cart\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace Ganyicz\Cart\Tests;
 
 class ExampleTest extends TestCase
 {
